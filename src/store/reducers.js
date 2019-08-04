eef51264ae9cea9f7d73b4657c19fd48f26c00d7
Rohit Islam <air.rohit999@gmail.com>
2019-08-04 06:47:51 +0000

    redux installed and store setup

---
+++ src/store/reducers.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 10
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
