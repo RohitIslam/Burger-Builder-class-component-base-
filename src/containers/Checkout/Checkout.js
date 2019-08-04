6525bdef15c511c9128479947725cd75b28b278a
Rohit Islam <air.rohit999@gmail.com>
2019-08-04 11:43:13 +0000

    checkout and contactData adjusted using redux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,31 +1,11 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import CheckoutSummery from "../../components/Orders/CheckoutSummery/CheckoutSummery";
 import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    totalPrice: 0
-  };
-
-  componentWillMount() {
-    const query = new URLSearchParams(this.props.location.search);
-    const extractedIngredients = {};
-    let price = 0;
-    for (let param of query.entries()) {
-      if (param[0] === "price") {
-        price = param[1];
-      } else {
-        // entries will be like ['salad', '1']
-        extractedIngredients[param[0]] = +param[1];
-      }
-    }
-
-    this.setState({ ingredients: extractedIngredients, totalPrice: price });
-  }
-
   checkoutCanceledHandler = () => {
     this.props.history.goBack();
   };
@@ -38,23 +18,24 @@ class Checkout extends Component {
     return (
       <div>
         <CheckoutSummery
-          ingredients={this.state.ingredients}
+          ingredients={this.props.ingredients}
           checkoutCanceled={this.checkoutCanceledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
         />
         <Route
           path={this.props.match.path + "/contact-data"}
-          render={props => (
-            <ContactData
-              ingredients={this.state.ingredients}
-              price={this.state.totalPrice}
-              {...props}
-            />
-          )}
+          component={ContactData}
         />
       </div>
     );
   }
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+export default connect(mapStateToProps)(Checkout);
